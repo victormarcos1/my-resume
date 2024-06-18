560124aaeb30c0d76d2e842d9c3c90bfcce4da0b
victormarcos1 <129120518+victormarcos1@users.noreply.github.com>
2024-06-18 00:43:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Example JavaScript for interactivity
+console.log('Resume loaded.');
+
+// Simple toggle for a collapsible sidebar
+document.getElementById('toggle-sidebar').addEventListener('click', function() {
+    document.querySelector('aside').classList.toggle('collapsed');
+});
